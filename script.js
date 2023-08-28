0bae0c81378c7cbf71b6303d839cc5fcf059558a
bentike <ajadiyusuf73@gmail.com>
2023-08-28 16:58:29 +0100

    wrote submitForm method to add data to table.

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // alert("connected");
+const userForm = document.getElementById("userForm");
+const membersTable = document.getElementById("membersList");
+const submit = document.getElementById("submit");
+
+let formData = new FormData(userForm);
+
+const submitForm = (event) => {
+   event.preventDefault();
+   let userForm = document.forms["users"];
+   let data = new FormData(userForm);
+   let newMemberRow = membersTable.insertRow();
+   for(let [key, value] of data){
+      newMemberRow.insertCell().innerHTML = data.get(key);
+   }
+}
+
+userForm.addEventListener("submit", submitForm);
