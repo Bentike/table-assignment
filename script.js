da6a9b85d3a336a4ad285f8cb8179d694709025a
bentike <ajadiyusuf73@gmail.com>
2023-08-28 22:35:32 +0100

    implemented color change

--- script.js
+++ script.js
@@ -2,17 +2,25 @@
 const userForm = document.getElementById("userForm");
 const membersTable = document.getElementById("membersList");
 const submit = document.getElementById("submit");
+const background = document.getElementById("bg");
 
 let formData = new FormData(userForm);
+let geekForm = document.forms["users"];
+
+
+const getColor = (event) => {
+    membersTable.style.backgroundColor = event.target.value;
+}
+
 
 const submitForm = (event) => {
-   event.preventDefault();
-   let userForm = document.forms["users"];
-   let data = new FormData(userForm);
-   let newMemberRow = membersTable.insertRow();
-   for(let [key, value] of data){
-      newMemberRow.insertCell().innerHTML = data.get(key);
-   }
+    event.preventDefault();
+    let data = new FormData(geekForm);
+    let newMemberRow = membersTable.insertRow();
+    for(let [key, value] of data){
+        newMemberRow.insertCell().innerHTML = data.get(key);
+    }
 }
 
 userForm.addEventListener("submit", submitForm);
+background.addEventListener("change", getColor, false);
